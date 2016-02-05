6697c747c3f3eda32703f998468025581335b8c7
David Neumann <david.neumann@lecode.de>
2016-02-05 22:18:58 +0100

    Added a simple console application sample.

---
+++ Samples/NineGag.Samples.Console/Program.cs
@@ -0,0 +1,65 @@
+﻿
+#region Using Directives
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace NineGag.Samples.Console
+{
+    /// <summary>
+    /// Represents the entry point to the sample application, which showcases the usage of 9GAG.NET.
+    /// </summary>
+    public class Program
+    {
+        #region Private Static Fields
+        
+        /// <summary>
+        /// Contains the NineGag client, which is used to retrieve information from 9GAG.
+        /// </summary>
+        private static readonly NineGagClient nineGagClient = new NineGagClient();
+
+        #endregion
+
+        #region Public Static Methods
+
+        /// <summary>
+        /// The entry point to the 9GAG.NET sample application.
+        /// </summary>
+        /// <param name="args">The command line arguments, which should be empty, because they are not used.</param>
+        public static void Main(string[] args) => Program.MainAsync().Wait();
+
+        /// <summary>
+        /// Represents the asynchronous entry point to the application, which makes it possible to call asynchronous methods.
+        /// </summary>
+        public static async Task MainAsync()
+        {
+            // Gets the first two pages of 9GAG
+            IEnumerable<Section> sections = await Program.nineGagClient.GetSectionsAsync();
+            Section hotSection = sections.FirstOrDefault(section => section.Kind == SectionKind.Hot);
+            List<Page> pages = new List<Page>();
+            pages.Add(await Program.nineGagClient.GetPostsAsync(hotSection));
+            pages.Add(await Program.nineGagClient.GetPostsAsync(hotSection, pages.Last()));
+
+            // Prints all the retrieved pages
+            foreach (Page page in pages)
+            {
+                System.Console.WriteLine();
+                System.Console.WriteLine($"Page {pages.IndexOf(page) + 1}");
+                System.Console.WriteLine();
+
+                foreach (Post post in page.Posts)
+                    System.Console.WriteLine(post.Title);
+            }
+
+            // Waits for a key stroke before the application is quit
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
--- Samples/NineGag.Samples.Console/Properties/AssemblyInfo.cs
+++ Samples/NineGag.Samples.Console/Properties/AssemblyInfo.cs
@@ -1,36 +1,29 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿
+#region Using Directives
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("NineGag.Samples.Console")]
-[assembly: AssemblyDescription("")]
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("9GAG.NET Console Sample")]
+[assembly: AssemblyDescription("A simple console sample, which show cases the usage of the 9GAG.NET library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NineGag.Samples.Console")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("9GAG.NET")]
+[assembly: AssemblyCopyright("Copyright © David Neumann 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("0504b635-45aa-446b-9f65-e5808d786fce")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#endregion
+
+#region Assembly Version Information
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+#endregion
